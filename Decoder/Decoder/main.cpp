9f9a7f59bcd9241806934ecb9f1548b83ccd0a48
Danyalo <danylolab@gmail.com>
2017-09-19 19:55:56 +0300

    Initial commit
    Created basic main.cpp

---
+++ Decoder/Decoder/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string path;
+	cout << "Hi! Please enter the path to your picture: \n";
+	cin >> path;
+
+	return 0;
+}
