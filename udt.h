f59893da449ca91dd1a37cb321072903efd4f61b
Max <maxym1998@gmail.com>
2017-09-28 20:20:56 +0300

    udt.h

---
+++ udt.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <windows.h>
+
+#ifndef UDT_H
+#define UDT_H
+
+// this is a struct for handling color vectors
+struct color_vector
+{
+	// rgb data
+	BYTE r;
+	BYTE g;
+	BYTE b;
+};
+
+#endif
